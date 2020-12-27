2633b7b95846d694c7763dec659bed26b84997bf
Shen Yaoxin <rafaelshen977@gmail.com>
2020-12-27 17:18:17 +0800

    save

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"go-webservice/handlers"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -15,5 +18,26 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", hh)
 
-	http.ListenAndServe(":9090", mux)
+	ss := http.Server{
+		Addr:         ":9090",
+		Handler:      mux,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		if err := ss.ListenAndServe(); err != nil {
+			l.Fatalln(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+	sig := <-sigChan
+
+	l.Println("Received terminate, graceful shutdown", sig)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	ss.Shutdown(tc)
 }
