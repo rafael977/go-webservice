bca0811dedc8ab82fbfe3ad163438a804f9384a7
Shen Yaoxin <rafaelshen977@gmail.com>
2020-12-27 23:06:57 +0800

    updates

--- handlers/Products.go
+++ handlers/Products.go
@@ -16,6 +16,14 @@ func NewProducts(l *log.Logger) *Products {
 }
 
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+	}
+
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	lp := data.GetProducts()
 	enc := json.NewEncoder(rw)
 	err := enc.Encode(lp)
